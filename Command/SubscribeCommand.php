f220d0a3dd4ce24daad7775394147810fccf93a8
Artem Henvald <genvaldartem@gmail.com>
2022-09-22 21:31:14 +0300

    Update subscribe console command

---
+++ Command/SubscribeCommand.php
@@ -0,0 +1,132 @@
+<?php
+/*
+ * This file is part of the FreshCentrifugoBundle.
+ *
+ * (c) Artem Henvald <genvaldartem@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Fresh\CentrifugoBundle\Command;
+
+use Fresh\CentrifugoBundle\Command\Argument\ArgumentChannelTrait;
+use Fresh\CentrifugoBundle\Command\Argument\ArgumentUserTrait;
+use Fresh\CentrifugoBundle\Command\Option\OptionClientTrait;
+use Fresh\CentrifugoBundle\Command\Option\OptionEpochTrait;
+use Fresh\CentrifugoBundle\Command\Option\OptionOffsetTrait;
+use Fresh\CentrifugoBundle\Command\Option\OptionSessionTrait;
+use Fresh\CentrifugoBundle\Model\StreamPosition;
+use Fresh\CentrifugoBundle\Service\CentrifugoChecker;
+use Fresh\CentrifugoBundle\Service\CentrifugoInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * SubscribeCommand.
+ *
+ * @author Artem Henvald <genvaldartem@gmail.com>
+ */
+#[AsCommand(name: 'centrifugo:subscribe', description: 'Subscribe user to a channel')]
+final class SubscribeCommand extends AbstractCommand
+{
+    use ArgumentChannelTrait;
+    use ArgumentUserTrait;
+    use OptionClientTrait;
+    use OptionEpochTrait;
+    use OptionOffsetTrait;
+    use OptionSessionTrait;
+
+    /**
+     * @param CentrifugoInterface $centrifugo
+     * @param CentrifugoChecker   $centrifugoChecker
+     */
+    public function __construct(CentrifugoInterface $centrifugo, protected readonly CentrifugoChecker $centrifugoChecker)
+    {
+        parent::__construct($centrifugo);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure(): void
+    {
+        $this
+            ->setDefinition(
+                new InputDefinition([
+                    new InputArgument('user', InputArgument::REQUIRED, 'User ID to subscribe'),
+                    new InputArgument('channel', InputArgument::REQUIRED, 'Name of channel to subscribe user to', null, $this->getChannelsForAutocompletion()),
+                    new InputOption('client', null, InputOption::VALUE_OPTIONAL, 'Specific client ID to subscribe (user still required to be set, will ignore other user connections with different client IDs)'),
+                    new InputOption('session', null, InputOption::VALUE_OPTIONAL, 'Specific client session to subscribe (user still required to be set)'),
+                    new InputOption('offset', null, InputOption::VALUE_OPTIONAL, 'Offset in a stream'),
+                    new InputOption('epoch', null, InputOption::VALUE_OPTIONAL, 'Stream epoch'),
+                ])
+            )
+            ->setHelp(
+                <<<'HELP'
+The <info>%command.name%</info> command allows subscribing user to a channel:
+
+<info>%command.full_name%</info> <comment>user123</comment> <comment>channelName</comment>
+
+You can subscribe specific user to a channel by client ID (user still required to be set):
+
+<info>%command.full_name%</info> <comment>user123</comment> <comment>channelName</comment> <comment>--client=clientID</comment>
+
+You can subscribe specific user to a channel by session ID (user still required to be set):
+
+<info>%command.full_name%</info> <comment>user123</comment> <comment>channelName</comment> <comment>--session=sessionID</comment>
+
+Read more at https://centrifugal.dev/docs/server/server_api#history
+HELP
+            )
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function initialize(InputInterface $input, OutputInterface $output): void
+    {
+        parent::initialize($input, $output);
+
+        $this->initializeUserArgument($input);
+        $this->initializeChannelArgument($input);
+        $this->initializeClientOption($input);
+        $this->initializeEpochOption($input);
+        $this->initializeOffsetOption($input);
+        $this->initializeSessionOption($input);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        try {
+            $this->centrifugo->subscribe(
+                user: $this->user,
+                channel: $this->channel,
+                client: $this->client,
+                session: $this->session,
+                recoverSince: new StreamPosition($this->offset, $this->epoch),
+            );
+            $io->success('DONE');
+        } catch (\Throwable $e) {
+            echo $e->getMessage();
+            $io->error($e->getMessage());
+
+            return self::FAILURE;
+        }
+
+        return self::SUCCESS;
+    }
+}
---
+++ Tests/Command/SubscribeCommandTest.php
@@ -0,0 +1,129 @@
+<?php
+/*
+ * This file is part of the FreshCentrifugoBundle.
+ *
+ * (c) Artem Henvald <genvaldartem@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Fresh\CentrifugoBundle\Tests\Command;
+
+use Fresh\CentrifugoBundle\Command\SubscribeCommand;
+use Fresh\CentrifugoBundle\Service\CentrifugoChecker;
+use Fresh\CentrifugoBundle\Service\CentrifugoInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * SubscribeCommandTest.
+ *
+ * @author Artem Henvald <genvaldartem@gmail.com>
+ */
+final class SubscribeCommandTest extends TestCase
+{
+    /** @var CentrifugoInterface|MockObject */
+    private CentrifugoInterface|MockObject $centrifugo;
+
+    /** @var CentrifugoChecker|MockObject */
+    private CentrifugoChecker|MockObject $centrifugoChecker;
+
+    private Command $command;
+    private Application $application;
+    private CommandTester $commandTester;
+
+    protected function setUp(): void
+    {
+        $this->centrifugo = $this->createMock(CentrifugoInterface::class);
+        $this->centrifugoChecker = $this->createMock(CentrifugoChecker::class);
+        $command = new SubscribeCommand($this->centrifugo, $this->centrifugoChecker);
+
+        $this->application = new Application();
+        $this->application->add($command);
+
+        $this->command = $this->application->find('centrifugo:subscribe');
+        $this->commandTester = new CommandTester($this->command);
+    }
+
+    protected function tearDown(): void
+    {
+        unset(
+            $this->centrifugo,
+            $this->centrifugoChecker,
+            $this->command,
+            $this->application,
+            $this->commandTester,
+        );
+    }
+
+    public function testSuccessfulExecutionWithRequiredParameters(): void
+    {
+        $this->centrifugo
+            ->expects(self::once())
+            ->method('subscribe')
+            ->with('user123', 'channelA')
+        ;
+
+        $result = $this->commandTester->execute(
+            [
+                'command' => $this->command->getName(),
+                'user' => 'user123',
+                'channel' => 'channelA',
+            ]
+        );
+        self::assertSame(0, $result);
+
+        $output = $this->commandTester->getDisplay();
+        self::assertStringContainsString('DONE', $output);
+    }
+
+    public function testSuccessfulExecutionWithAllParameters(): void
+    {
+        $this->centrifugo
+            ->expects(self::once())
+            ->method('subscribe')
+            ->with('user123', 'channelA')
+        ;
+
+        $result = $this->commandTester->execute(
+            [
+                'command' => $this->command->getName(),
+                'user' => 'user123',
+                'channel' => 'channelA',
+                '--client' => 'clientID',
+                '--session' => 'sessionID',
+            ]
+        );
+        self::assertSame(0, $result);
+
+        $output = $this->commandTester->getDisplay();
+        self::assertStringContainsString('DONE', $output);
+    }
+
+    public function testException(): void
+    {
+        $this->centrifugo
+            ->expects(self::once())
+            ->method('subscribe')
+            ->willThrowException(new \Exception('test'))
+        ;
+
+        $result = $this->commandTester->execute(
+            [
+                'command' => $this->command->getName(),
+                'user' => 'user123',
+                'channel' => 'channelA',
+            ]
+        );
+        self::assertSame(1, $result);
+
+        $output = $this->commandTester->getDisplay();
+        self::assertStringContainsString('test', $output);
+    }
+}
